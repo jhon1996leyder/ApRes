5856987e4b2507bc8b98507466e505b4713476e4
jhon1996leyder <08jhonleider@gmail.com>
2022-10-25 08:54:28 -0500

    make it better

--- ApiRest/wsgi.py
+++ ApiRest/wsgi.py
@@ -10,5 +10,7 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 import os  
 from django.core.wsgi import get_wsgi_application  
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ApiRest.settings")  
-application = get_wsgi_application() 
+application = get_wsgi_application()  
+from whitenoise.django import DjangoWhiteNoise  
+application = DjangoWhiteNoise(application)
 
